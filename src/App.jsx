db0bea20bbbc15aba08a0fd3efd1cd83ca0a11a2
GusMartin91 <gustavomlozano@gmail.com>
2023-09-25 22:23:18 -0300

    Iniciando Proyecto ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/layout/navbar/Navbar";
+import { Home } from "./components/pages/home/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <h4>Logo</h4>
+      <ul>
+        <li>Todos</li>
+        <li>Acero Templado</li>
+        <li>Hierro Dulce</li>
+        <li>Otros</li>
+      </ul>
+      WIDGET CARRITO
+    </div>
+  );
+};
